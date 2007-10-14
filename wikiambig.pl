bc68c9be6daf00778a029a515bf93a746df1681a
avian <avian@19fbb8fb-b300-474b-854f-3758a290f079>
2007-10-14 13:26:06 +0000

    Collect disambig links
    git-svn-id: svn+ssh://svn.zemanta.com/zdev/neith/wikiprep@618 19fbb8fb-b300-474b-854f-3758a290f079

--- wikiambig.pl
+++ wikiambig.pl
@@ -41,6 +41,7 @@ if (@ARGV < 1) {
 }
 
 my $file;
+
 my $showLicense = 0;
 my $showVersion = 0;
 
@@ -71,6 +72,8 @@ if (! -e $file) {
 
 ##### Global variables #####
 
+my %namespaces;
+
 my ($fileBasename, $filePath, $fileSuffix) = fileparse($file, ".xml");
 my $disambigPagesFile = "$filePath/$fileBasename.disambig";
 
@@ -86,11 +89,85 @@ print DISAMBIGF  "# Line format: <Disambig page id>\n\n";
 
 print "Counting pages...\n";
 
+&loadNamespaces();
 &prescan();
 
 &transform();
 
 close(DISAMBIGF);
+
+##### Subroutines #####
+
+sub normalizeTitle(\$) {
+  my ($refToStr) = @_;
+
+  # remove leading whitespace and underscores
+  $$refToStr =~ s/^[\s_]+//;
+  # remove trailing whitespace and underscores
+  $$refToStr =~ s/[\s_]+$//;
+  # replace sequences of whitespace and underscore chars with a single space
+  $$refToStr =~ s/[\s_]+/ /g;
+
+  if ($$refToStr =~ /^([^:]*):(\s*)(\S(?:.*))/) {
+    my $prefix = $1;
+    my $optionalWhitespace = $2;
+    my $rest = $3;
+
+    my $namespaceCandidate = $prefix;
+    &normalizeNamespace(\$namespaceCandidate); # this must be done before the call to 'isKnownNamespace'
+    if ( &isKnownNamespace(\$namespaceCandidate) ) {
+      # If the prefix designates a known namespace, then it might follow by optional
+      # whitespace that should be removed to get the canonical page name
+      # (e.g., "Category:  Births" should become "Category:Births").
+      $$refToStr = $namespaceCandidate . ":" . ucfirst($rest);
+    } else {
+      # No namespace, just capitalize first letter.
+      # If the part before the colon is not a known namespace, then we must not remove the space
+      # after the colon (if any), e.g., "3001: The_Final_Odyssey" != "3001:The_Final_Odyssey".
+      # However, to get the canonical page name we must contract multiple spaces into one,
+      # because "3001:   The_Final_Odyssey" != "3001: The_Final_Odyssey".
+      $$refToStr = ucfirst($prefix) . ":" .
+                   (length($optionalWhitespace) > 0 ? " " : "") . $rest;
+    }
+  } else {
+    # no namespace, just capitalize first letter
+    $$refToStr = ucfirst($$refToStr);
+  }
+}
+
+sub normalizeNamespace(\$) {
+  my ($refToStr) = @_;
+
+  $$refToStr = ucfirst( lc($$refToStr) );
+}
+
+# Checks if the prefix of the page name before the colon is actually one of the
+# 16+2+2 namespaces defined in the XML file.
+# Assumption: the argument was already normalized using 'normalizeNamespace'
+sub isKnownNamespace(\$) {
+  my ($refToStr) = @_;
+
+  defined( $namespaces{$$refToStr} );  # return value
+}
+
+sub loadNamespaces() {
+  # re-open the input XML file
+  my $pages = Parse::MediaWikiDump::Pages->new($file);
+
+  # load namespaces
+  my $refNamespaces = $pages->namespaces;
+
+  # namespace names are case-insensitive, so we force them
+  # to canonical form to facilitate future comparisons
+  my $ns;
+  foreach $ns (@$refNamespaces) {
+    my @namespaceData = @$ns;
+    my $namespaceId   = $namespaceData[0];
+    my $namespaceName = $namespaceData[1];
+    &normalizeNamespace(\$namespaceName);
+    $namespaces{$namespaceName} = $namespaceId;
+  }
+}
 
 # Count pages
 sub prescan() {
@@ -149,10 +226,10 @@ sub transform() {
 			)
 		\}\})/ix ) {
       # print "BODY $id : $title : $1\n";
-      &markDisambig(\$id);
+      &parseDisambig(\$id, \$text);
     } elsif ( $title =~ /\(disambiguation\)/ ) {
        # print "TITLE $id : $title : $1\n";
-      &markDisambig(\$id);
+      &parseDisambig(\$id, \$text);
     }
 
     my $nowTime = time;
@@ -167,10 +244,135 @@ sub transform() {
   }
 }
 
-sub markDisambig(\$) {
-	my ($refToid) = @_;
+sub parseDisambig(\$\$) {
+	my ($refToId, $refToText) = @_;
 
-	print DISAMBIGF "$$refToid\n";
+	my @disambigLinks;
+
+	my @lines = split(/\n/, $$refToText);
+	my $line;
+
+	for $line (@lines) {
+		if ( $line =~ /^\s*(?:
+					(\*\*)|
+					(\#\#)|
+					(\:\#)|
+					(\:\*)|
+					(\#)|
+					(\*)
+				)/ix ) {
+
+			&extractInternalLinks(\$line, \@disambigLinks);
+		}
+	}
+
+	&markDisambig($refToId, \@disambigLinks);
+}
+
+sub extractInternalLinks(\$\@) {
+  my ($refToText, $refToInternalLinksArray) = @_;
+
+  # Link definitions may span over adjacent lines and therefore contain line breaks,
+  # hence we use the /s modifier.
+  # Occasionally, links are nested, e.g.,
+  # [[Image:kanner_kl2.jpg|frame|right|Dr. [[Leo Kanner]] introduced the label ''early infantile autism'' in [[1943]].]]
+  # In order to prevent incorrect parsing, e.g., "[[Image:kanner_kl2.jpg|frame|right|Dr. [[Leo Kanner]]",
+  # we extract links in several iterations of the while loop, while the link definition requires that
+  # each pair [[...]] does not contain any opening braces.
+
+  1 while ( $$refToText =~ s/
+                             \[\[
+                                   ([^\[]*?)  # the link text can be any chars except an opening bracket,
+                                              # this ensures we correctly parse nested links (see comments above)
+                             \]\]
+                            /&collectInternalLink($1, $refToInternalLinksArray)/segx
+          );
+}
+
+sub collectInternalLink($$$\@\@) {
+  my ($link, $refToInternalLinksArray) = @_;
+
+  my $originalLink = $link;
+  my $result = "";
+
+  # strip leading whitespace, if any
+  $link =~ s/^\s*//;
+
+  # Link definitions may span over adjacent lines and therefore contain line breaks,
+  # hence we use the /s modifier on most matchings.
+
+  # There are some special cases when the link may be preceded with a colon.
+  # Known cases:
+  # - Linking to a category (as opposed to actually assigning the current article
+  #   to a category) is performed using special syntax [[:Category:...]]
+  # - Linking to other languages, e.g., [[:fr:Wikipedia:Aide]]
+  #   (without the leading colon, the link will go to the side menu
+  # - Linking directly to the description page of an image, e.g., [[:Image:wiki.png]]
+  # In all such cases, we strip the leading colon.
+  if ($link =~ /^
+                   :        # colon at the beginnning of the link name
+                   (.*)     # the rest of the link text
+                $
+               /sx) {
+    # just strip this initial colon (as well as any whitespace preceding it)
+    $link = $1;
+  }
+
+  # Alternative text may be available after the pipeline symbol.
+  # If the pipeline symbol is only used for masking parts of
+  # the link name for presentation, we still consider that the author of the page
+  # deemed the resulting text important, hence we always set this variable when
+  # the pipeline symbol is present.
+  my $alternativeTextAvailable = 0;
+
+  # Some links contain several pipeline symbols, e.g.,
+  # [[Image:Zerzan.jpeg|thumb|right|[[John Zerzan]]]]
+  # It seems that the extra pipeline symbols are parameters, so we just eliminate them.
+  if ($link =~ /^(.*)\|([^|]*)$/s) { # first, extract the link up to the last pipeline symbol
+    $link = $1;    # the part before the last pipeline
+    $result = $2;  # the part after the last pipeline, this is usually an alternative text for this link
+
+    $alternativeTextAvailable = 1; # pipeline found, see comment above
+
+    # Now check if there are pipeline symbols remaining.
+    # Note that this time we're looking for the shortest match,
+    # to take the part of the text up to the first pipeline symbol.
+    if ($link =~ /^([^|]*)\|(.*)$/s) {
+      $link = $1;
+      # $2 contains the parameters, which we don't really need
+    }
+
+  } else {
+    # the link text does not contain the pipeline, so take it as-is
+    $result = $link;
+  }
+
+  if ($link =~ /^(.*)\#(.*)$/s) {
+    # The link contains an anchor, so adjust the link to point to the page as a whole.
+    $link = $1;
+    my $anchor = $2;
+    # Check if the link points to an anchor on the current page, and if so - ignore it.
+    if (length($link) == 0 && ! $alternativeTextAvailable) {
+      # This is indeed a link pointing to an anchor on the current page.
+      # The link is thus cleared, so that it will not be resolved and collected later.
+      # For anchors to the same page, discard the leading '#' symbol, and take
+      # the rest as the text - but only if no alternative text was provided for this link.
+      $result = $anchor;
+    }
+  }
+
+  &normalizeTitle(\$link);
+  push(@$refToInternalLinksArray, $link);
+
+  $result;  #return value
+}
+
+sub markDisambig(\$\@) {
+	my ($refToDisambigId, $refToTitle) = @_;
+
+	my $titles = join('\t', @$refToTitle); 
+
+	print DISAMBIGF "$$refToDisambigId\t$titles\n";
 }
 
 sub printUsage()
