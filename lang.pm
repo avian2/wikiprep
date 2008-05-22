c3a3cc7ac16fc1a2c6cc0e9be0ff2b5a2fa51947
tadej <tadej@19fbb8fb-b300-474b-854f-3758a290f079>
2008-05-22 09:18:22 +0000

    Added slovene-specific regexes and constants.
    git-svn-id: svn+ssh://svn.zemanta.com/zdev/neith2/wikiprep/trunk@6253 19fbb8fb-b300-474b-854f-3758a290f079

--- lang.pm
+++ lang.pm
@@ -116,6 +116,107 @@ my $langDB = {
 
                   'disambigTitle' =>
                       qr/\(disambiguation\)/ix,
+              },
+
+              'sl' => {
+                  # Names of months as used by MediaWiki for date links
+                  
+                  'numberToMonth' => {
+                      1 => 'Januuar', 
+                      2 => 'Februar', 
+                      3 => 'Marec', 
+                      4 => 'April',
+                      5 => 'Maj', 
+                      6 => 'Junij', 
+                      7 => 'Julij', 
+                      8 => 'Avgust', 
+                      9 => 'September', 
+                      10 => 'Oktober', 
+                      11 => 'November', 
+                      12 => 'December'
+                  },
+    
+                  # Following three are pre-compiled regular expressions used for recognizing links to 
+                  # related articles.
+                  
+                  # We require that stanalone designators occur at the beginning of the line
+                  # (after at most a few characters, such as a whitespace or a colon),
+                  # and not just anywhere in the line. Otherwise, we would collect as related
+                  # those links that just happen to occur in the same line with an unrelated
+                  # string that represents a standalone designator.
+                  
+                  'relatedWording_Standalone' =>
+                      qr/Glavni(?:\s+)član(ek|ki)|
+                         Dodatne(?:\s+)informacije|
+                         Povezani(?:\s+)član(ek|ki)|
+                         Povezan(a|e)(?:\s+)tem(a|e)|
+                         Glej(?:\s+)glavn(i|e)(?:\s+)član(ek|ke)|
+                         Glej(?:\s+)član(ek|ke)|
+                         Glej(?:\s+)tudi|
+                         Za(?:\s+)(?:več|dodatn[io]|nadaljn[io])/ix,
+                  ## For(?:\s+)more(?:\s+)(?:background|details)(?:\s+)on(?:\s+)this(?:\s+)topic,(?:\s+)see
+                  
+                  # Can appear anywhere in text, but must be enclosed in parentheses.
+
+                  'relatedWording_Inline' =>
+                      qr/Glej[\s:]|
+                         Glej(?:\s+)tudi|
+                         Za(?:\s+)(?:več|dodatn[io]|nadaljn[io])/ix,
+
+                  # Article sections containing links to related articles.
+
+                  'relatedWording_Section' => 
+                      qr/Dodatne(?:\s+)informacije|
+                         Glej(?:\s+)tudi|
+                         Povezani?(?:\s+)član(ek|ki)|
+                         Povezan[ea](?:\s+)tem[ea]/ix,
+
+                  # We only process pages in these namespaces + the main namespace (which has an empty name)
+
+                  'okNamespacesForPrescanning' => {
+                      'Predloga' => 1, 
+                      'Kategorija' => 1, 
+                      'Slika' => 1
+                  },
+
+                  # Pages in these namespaces end up in the final hgw.xml file.
+
+                  'okNamespacesForTransforming' => {
+                      'Kategorija' => 1, 
+                      'Slika' => 1
+                  },
+
+                  # Pages in these namespaces that don't exist in the XML dump but have a link to it, 
+                  # get assigned a local ID. Broken links to other pages get ignored.
+
+                  'okNamespacesForLocalPages' => {
+                      'Slika' => 1
+                  },
+
+                  # Namespace for categories.
+
+                  'categoryNamespace' =>
+                      'Kategorija',
+
+                  # Namespace for templates.
+
+                  'templateNamespace' =>
+                      'Predloga',
+
+                  # namespace for images.
+
+                  'imageNamespace' =>
+                      'Slika',
+
+                  # Regular expression that matches names of templates that mark disambiguation articles.
+
+                  'disambigTemplates' =>
+                      qr/razločitev/ix,
+
+                  # Regular expression that matches titles of disambiguation articles.
+
+                  'disambigTitle' =>
+                      qr/\(razločitev\)/ix,
               }
           };
 
