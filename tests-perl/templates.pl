36e08309791183cf90b5568dbea0f2a056d2e12b
avian <avian@19fbb8fb-b300-474b-854f-3758a290f079>
2008-08-06 14:40:27 +0000

    Added unit test for unmatched [ in template invocation.
    git-svn-id: svn+ssh://svn.zemanta.com/zdev/neith2/wikiprep/trunk@8281 19fbb8fb-b300-474b-854f-3758a290f079

--- tests-perl/templates.pl
+++ tests-perl/templates.pl
@@ -1,4 +1,4 @@
-use Test::More tests => 16;
+use Test::More tests => 18;
 use templates;
 
 my $r;
@@ -61,3 +61,14 @@ is($paramHash{'=3='}, "bare_param");
 is($paramHash{'1'}, "[[link|anchor]]");
 is($paramHash{'2'}, "{{nested|{{template|p}}\n|blah}}");
 is($paramHash{'3'}, "bare_param");
+
+$text = "Infobox_University\n|name          = Uppsala University\n|native_name   = Uppsala universitet\n|latin_name    = Universitas Regia Upsaliensis'', also ''Academia Regia Upsaliensis\n|image_name    = Uppsala University seal.png\n|motto         = Gratiae veritas naturae ([[Latin]])\n|mottoeng      = Truth through God's mercy and nature\n|established   = 1477\n|type          = [[Public university|public]]\n|city          = {{Flagicon|Sweden}} [[Uppsala]]\n|country       = [[Sweden]]\n|endowment = 4,319 million [[SEK]] per year ''(2007)'' &lt;/br&gt; (ca. 460 million [[Euro|EUR]] or 715 million [[US dollars|USD]])&lt;ref&gt;[http://info.uu.se/uadm/dokument.nsf/enhet/B56328B60271A22CC1256F400047D809/\$file/AR07.pdf Årsredovisning 2007&quot;&lt;/ref&gt;\n|enrollment    = 30,450&lt;ref&gt;Högskoleverket: [http://www.hsv.se/download/18.5b73fe55111705b51fd80004587/0733R.pdf Universitet &amp; Högskolor. Högskoleverkets årsrapport 2007] ISSN 1400-948X&lt;/ref&gt;\n|undergrad     = \n|postgrad      =\n|doctoral      = 2,400\n|staff         = 6,000 &lt;/br&gt; (3,800 teaching)\n|head_label    = [[Rector magnificus]] and [[Vice Chancellor]]\n|head          = Prof. [[Anders Hallberg]]\n|campus        =\n|colours       = {{color box|#990000}} {{color box|#FFFFFF}} [[Maroon (color)|maroon]], [[White (color)|white]]\n|mascot        =\n|affiliations  = [[Coimbra Group]]&lt;br\&gt; [[European University Association|EUA]]\n|free_label    =\n|free          =\n|website       = http://www.uu.se/\n";
+$name = '';
+%paramHash = ();
+
+&templates::parseTemplateInvocation(\$text, \$name, \%paramHash);
+is($name, "Infobox_University\n");
+
+# There is an unmatched [ in there that breaks template parsing.
+#is($paramHash{'website'}, 'http://www.uu.se');
+is($paramHash{'website'}, undef);
