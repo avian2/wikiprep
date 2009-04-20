e682216f7e9ba0914f1e203cc2ccef4c06f9eccd
avian <avian@19fbb8fb-b300-474b-854f-3758a290f079>
2009-04-20 09:17:25 +0000

    Split wiki tool.
    git-svn-id: svn+ssh://svn.zemanta.com/zdev/neith2/wikiprep/trunk@14839 19fbb8fb-b300-474b-854f-3758a290f079

---
+++ tools/splitwiki/splitwiki.c
@@ -0,0 +1,99 @@
+/* SpliWiki, split Wikipedia dump into multiple files                      */
+/* Copyright (C) 2009 Tomaz Solc                                           */
+
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License version 2 as       */
+/* published by the Free Software Foundation.                              */
+
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software             */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAXLINELEN	(5 * 1024 * 1024)
+
+#define PAGE_START	"  <page>\n"
+#define PAGE_END	"  </page>\n"
+
+void syntax() 
+{
+	printf("SYNTAX: splitwiki num_split prefix\n");
+	exit(0);
+}
+
+void split(int num_split, FILE *files[]) {
+
+	int file = -1;
+
+	char *buffer, *r;
+
+	buffer = malloc(MAXLINELEN*sizeof(*buffer));
+
+	while(1) {
+		r=fgets(buffer, MAXLINELEN, stdin);
+
+		if(r==NULL) return;
+
+		if(!strcmp(buffer, PAGE_START)) {
+			file = random()%num_split;
+		}
+
+		if(file == -1) {
+			int n;
+			for(n = 0; n < num_split; n++) fputs(buffer, files[n]);
+		} else {
+			fputs(buffer, files[file]);
+
+		}
+
+		if(!strcmp(buffer, PAGE_END)) {
+			file = -1;
+		}
+	}
+
+	free(buffer);
+}
+
+int main(int argc, char *argv[]) {
+	int n;
+
+	if(argc != 3) syntax();
+
+	int num_split;
+	n = sscanf(argv[1], "%d", &num_split);
+	if( n != 1 ) syntax();
+
+	char *prefix = argv[2];
+
+	FILE **files = malloc(sizeof(*files) * num_split);
+	for(n = 0; n < num_split; n++) {
+		char filename[1024];
+		sprintf(filename, "%s%04d", prefix, n);
+
+		files[n] = fopen(filename, "w");
+
+		if( files[n] == NULL ) {
+			printf("%s: %s\n", filename, strerror(errno));
+			return 1;
+		}
+	}
+
+	split(num_split, files);
+
+	for(n = 0; n < num_split; n++) {
+		fclose(files[n]);
+	}
+
+	free(files);
+
+	return 0;
+}
