9f140af70f9e12eb0b3c78ca0485fcab54424ded
Tomaz Solc <tomaz.solc@tablix.org>
2009-10-22 16:57:15 +0200

    Removed git and svn revision detection code.

--- lib/Wikiprep/revision.pm
+++ lib/Wikiprep/revision.pm
@@ -4,41 +4,8 @@ package Wikiprep::revision;
 
 use strict;
 use Exporter 'import';
-our @EXPORT_OK = qw( getWikiprepRevision getDumpDate getDatabaseName );
+our @EXPORT_OK = qw( getDumpDate getDatabaseName );
 use FindBin;
-
-sub getWikiprepRevision() {
-  my $version;
-
-  my $topdir = $FindBin::Bin;
-  $topdir =~ s/t\/?//;
-
-  # First try SVN...
-  if( open(VERSION, "svnversion $topdir|") ) {
-    $version = <VERSION>;
-    chomp($version);
-    close(VERSION);
-  } else {
-    $version = "exported";
-  }
-
-  if( $version eq "exported" ) {
-    # No SVN version information. Check for git.
-    if( open(VERSION, "git --git-dir $topdir/.git rev-parse HEAD|") ) {
-      $version = <VERSION>;
-      if( defined( $version ) ) {
-        chomp($version);
-        close(VERSION);
-      } else {
-        $version = "unknown";
-      }
-    } else {
-      $version = "unknown";
-    }
-  }
-
-  return $version;
-}
 
 sub getDumpDate($) {
   my ($dumpFile) = @_;
--- t/revision.t
+++ t/revision.t
@@ -1,8 +1,5 @@
-use Test::Simple tests => 4;
-use Wikiprep::revision qw( getWikiprepRevision getDumpDate );
-
-$svnrev = &getWikiprepRevision();
-ok($svnrev =~ /^\w+$/, "revision '$svnrev'");
+use Test::Simple tests => 3;
+use Wikiprep::revision qw( getDumpDate );
 
 $wikirev = &getDumpDate("/var/wikipedia/enwiki-20080103rev1-pages-articles.xml");
 ok($wikirev eq "20080103rev1");
